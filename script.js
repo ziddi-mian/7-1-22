7599ed63731934524d03e840f2819acadea7dc14
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-07 14:46:47 +0500

    jss

--- script.js
+++ script.js
@@ -1414,3 +1414,32 @@ function root(a,b,c) {
     }
   }
   root(1,5,5)
+
+
+
+
+
+   // program 8
+  // Write a js program to create Simple Calculator using switch case.
+  function calculator(num1,num2,x) {
+    switch (x) {
+      case '+':
+      console.log( num1+num2);
+      break;
+      case '-':
+        console.log(num1-num2);
+        break;
+      case '*':
+        console.log(num1*num2);
+        break;
+      case '/':
+        console.log(num1/num2);
+        break;
+    
+      default:
+        console.log("Enter Valid Operation");
+        break;
+    }
+    
+  }
+  calculator(1,2,'+')
