fa67b2f69fa54e4d756e6eaf0a0421c90dae994c
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-07 14:17:57 +0500

    js

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function lengthOfString() {
     console.log("Length Of String Is ", lengthString);
   }
   lengthOfString();
+
+
+   // program 2
+// Write a js program to copy one string to another string.
+function copyString() {
+    var String = "Muhammad Usama";
+    var copy = "";
+    var lengthof = String.length;
+    lengthof = lengthof / 2;
+    var first = String.slice(0, lengthof);
+    var second = String.slice(lengthof + 1, String.length);
+    copy = first + second;
+    console.log("Copy Of String Is:", copy);
+  }
+  copyString();
