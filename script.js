fc340e510658730a6d6b5cb66ed20e79f6d49f8e
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-07 14:17:34 +0500

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+// program 1
+// Write a js program to find length of a string.
+
+function lengthOfString() {
+    var string = "my name is usama";
+    var lengthString = string.length;
+    console.log("Length Of String Is ", lengthString);
+  }
+  lengthOfString();
