61ff792c288f8bdf816a616be83bb60e80543f5f
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-07 14:43:11 +0500

    js

--- script.js
+++ script.js
@@ -1070,3 +1070,14 @@ function subset() {
     
   }
   Areapara(10)
+
+
+  
+  // program 48
+  // Write a JavaScript function to print all the methods in an JavaScript object.
+  
+  function objectname(any) {
+    return Object.getOwnPropertyNames(any)
+    
+  }
+  console.log(objectname(Array));
