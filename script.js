3e75ed0938340f56ae1c49faa5337fd8326d9cbf
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-07 14:41:27 +0500

    js

--- script.js
+++ script.js
@@ -909,3 +909,27 @@ function lasttWord(f) {
     
   }
   properties()
+
+
+
+
+   // program 40
+  // Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+  // Sample object:
+  // var student = {
+  // name : "David Rayy",
+  // sclass : "VI",
+  // rollno : 12 };
+  
+  function deleteobj() {
+    var student={
+      name:"David Rayy",
+      sclass:"VI",
+      rollno:12,
+    };
+    console.log("Before Deletion",student);
+    delete student.rollno
+    console.log("After Deletion",student);
+    
+  }
+  deleteobj()
